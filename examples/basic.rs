8ec22edd2203f5363e567783b661bda77f5c35a6
Masterchef365 <duncan.freeman1@gmail.com>
2024-11-19 23:56:02 -0800

    idk im doin stuff

---
+++ examples/basic.rs
@@ -0,0 +1,17 @@
+use egui::{CentralPanel, ScrollArea};
+use egui_spreadsheet::SpreadSheetWidget;
+
+fn main() {
+    eframe::run_simple_native("Demo Sheet", Default::default(), move |ctx, _frame| {
+        CentralPanel::default().show(ctx, |ui| {
+            ScrollArea::both().show_viewport(ui, |ui, viewport| {
+                SpreadSheetWidget::new(10, 10)
+                    .show_area(viewport)
+                    .show_persisted_meta(ui, |ui, (col, row)| {
+                        ui.label(format!("I'm a cell at ({col}, {row})"));
+                    });
+            });
+        });
+    })
+    .unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use egui::{Id, Pos2, Rect, Sense, Ui, Vec2, Widget};
+use egui::{Id, Pos2, Rect, ScrollArea, Sense, Ui, Vec2, Widget};
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct SpreadSheetWidget {
     dimension: (usize, usize),
-    //id_salt: Option<Id>,
+    show_area: Option<Rect>,
+    id_salt: Option<Id>,
 }
 
 #[derive(Default, Clone)]
@@ -15,7 +16,10 @@ pub struct SpreadsheetMetadata {
 
 impl SpreadsheetMetadata {
     pub fn total_internal_size(&self) -> Vec2 {
-        Vec2::new(self.column_widths.total_width(), self.row_heights.total_width())
+        Vec2::new(
+            self.column_widths.total_width(),
+            self.row_heights.total_width(),
+        )
     }
 }
 
@@ -36,18 +40,51 @@ struct SelectionDetails {
 */
 
 impl SpreadSheetWidget {
-    fn show(
-        self,
+    pub fn new(cols: usize, rows: usize) -> Self {
+        Self {
+            dimension: (cols, rows),
+            show_area: None,
+            id_salt: None,
+        }
+    }
+
+    pub fn show_area(mut self, area: Rect) -> Self {
+        self.show_area = Some(area);
+        self
+    }
+
+    pub fn show(
+        &mut self,
         ui: &mut Ui,
         meta: &mut SpreadsheetMetadata,
-        view_sub_area: Rect,
-        cell_ui: impl FnMut(&mut Ui, (usize, usize)) -> egui::Response,
+        cell_ui: impl FnMut(&mut Ui, (usize, usize)),
     ) -> egui::Response {
+        // Synchronize the width numbering
+        let (cols, rows) = self.dimension;
+        meta.row_heights.set_len(rows);
+        meta.column_widths.set_len(cols);
+
         let resp = ui.allocate_response(meta.total_internal_size(), Sense::click_and_drag());
 
-        if resp.clicked() {
-        }
+        if resp.clicked() {}
 
+        resp
+    }
+
+    /// Same as using SpreadSheetWidget directly, but SpreadsheetMetadata is handled using egui.
+    pub fn show_persisted_meta(
+        &mut self,
+        ui: &mut Ui,
+        cell_ui: impl FnMut(&mut Ui, (usize, usize)),
+    ) -> egui::Response {
+        let id: Id = ui.next_auto_id().with(self.id_salt);
+        let mut meta = ui.memory_mut(|w| {
+            w.data
+                .get_persisted_mut_or_default::<SpreadsheetMetadata>(id)
+                .clone()
+        });
+        let resp = self.show(ui, &mut meta, cell_ui);
+        ui.memory_mut(|w| *w.data.get_persisted_mut_or_default(id) = meta);
         resp
     }
 }
@@ -60,9 +97,12 @@ pub struct SpreadsheetWidths {
 }
 
 impl SpreadsheetWidths {
-    pub fn get_width(&mut self, idx: usize) -> f32 {
-        self.widths.resize(idx+1, self.default_width);
-        self.widths[idx]
+    pub fn set_len(&mut self, len: usize) {
+        self.widths.resize(len, self.default_width);
+    }
+
+    pub fn get_width(&self, idx: usize) -> Option<f32> {
+        self.widths.get(idx).copied()
     }
 
     pub fn set_width(&mut self, idx: usize, width: f32) {
